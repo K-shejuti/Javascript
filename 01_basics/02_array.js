698a757a63e32cb0eaf3716272e0a72c7a9e2e79
K-shejuti <152790020+K-shejuti@users.noreply.github.com>
2025-06-02 17:41:10 +0000

    array

---
+++ 01_basics/02_array.js
@@ -0,0 +1,17 @@
+const myarr=[0,1,2,3,4]
+const myarr2=["esha","sweta","ruma"]
+let all_heroes=myarr.concat(myarr2)
+// console.log(myarr)
+// myarr.push(myarr2)
+console.log(myarr)
+// console.log(all_heroes)
+// let all_new_heroes=[...myarr,...myarr2]
+// console.log(all_new_heroes)
+// const another_arr=[1,2,3,[4,5,6],[7,8,[9,10]]]
+// const usuable_arr=another_arr.flat(Infinity)
+// console.log(usuable_arr)
+console.log(Array.from("Shejuti"))
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
