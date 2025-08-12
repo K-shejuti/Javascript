d7fea492e9062500b33656b0a44dc81ba7bc6a4b
K-shejuti <152790020+K-shejuti@users.noreply.github.com>
2025-08-12 15:17:21 +0000

    IIFE

---
+++ 03_basics/IIFE.js
@@ -0,0 +1,14 @@
+//named IIFIE
+(function chai(){
+    console.log("DB Connected")
+})();
+//unnamed IIFIE
+(() =>{
+    console.log("db connected tow")
+})();
+
+((name) =>{
+    console.log(`db connected tow  ${name}`)
+})("shejuti")
+
+
